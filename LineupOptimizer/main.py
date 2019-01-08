c32dc7b3fba638819a309e1df90079bf45abd445
deankou <34410352+deankou@users.noreply.github.com>
2019-01-07 22:02:56 -0600

    Actually cleaned up comments

--- LineupOptimizer/main.py
+++ LineupOptimizer/main.py
@@ -10,36 +10,25 @@ cwd = os.getcwd()
 salaryPath = cwd + "\DKSalaries.csv"
 
 ###Removed to allow for speed in testing
-##listofinactive = scrapeInactives.scrape()
+inactives = scrapeInactives.scrape()
 
 # Set up the optimizer
 optimizer = get_optimizer(Site.DRAFTKINGS, Sport.BASKETBALL)
 optimizer.load_players_from_csv(salaryPath)
 
-##inactives = []
-##inactives.append("Lebron James")
-##inactives.append("Rajon Rondo")
-
+### Manual remove players
 ##alsoRemove = ["Lebron James",
 ##              "Rajon Rondo"]
-##
 ##for players in alsoRemove:
 ##    inactives.append(players)
 
+injuries = []
+for player in inactives:
+    injuries.append(optimizer.get_player_by_name(player))
 
-
-##injuries = []
-##for player in inactives:
-##    injuries.append(optimizer.get_player_by_name(player))
-
-##for player in injuries:
-##    optimizer.remove_player(player)
-
-##bron = optimizer.get_player_by_name("Lebron James")
-player = optimizer.get_player_by_name('Lebron James')
-optimizer.remove_player(player)
+for player in injuries:
+    optimizer.remove_player(player)
     
-    
-for lineup in optimizer.optimize(n=1, max_exposure=0.3):
+for lineup in optimizer.optimize(n=3, max_exposure=0.3):
     print(lineup)
     
